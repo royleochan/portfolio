827e7f379ca52cc32030a309179e0c640e7eefcf
royleochan <royleochan@gmail.com>
2020-08-06 01:29:08 +0800

    Create education page

---
+++ src/pages/education.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react"
+
+import Toolbar from "components/Header/Toolbar/Toolbar.jsx"
+import Footer from "components/Footer/Footer.jsx"
+import Backdrop from "components/Backdrop/Backdrop.jsx"
+import SideDrawer from "components/Header/SideDrawer/SideDrawer.jsx"
+
+const Education = props => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen)
+  }
+
+  const backdropClickHandler = () => {
+    toggleDrawer()
+  }
+
+  return (
+    <div style={{ height: "100%" }}>
+      <Toolbar openDrawer={toggleDrawer} />
+      {isDrawerOpen && <SideDrawer show={isDrawerOpen} />}
+      {isDrawerOpen && <Backdrop click={backdropClickHandler} />}
+      <main style={{ marginTop: "6rem" }}>
+        <h1>Education</h1>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Education
