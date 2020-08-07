69a3c209fd087696487f18c7d1f64546bf5d008d
royleochan <royleochan@gmail.com>
2020-08-08 03:08:42 +0800

    Design contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,9 @@
 import React, { useState } from "react"
+import { useForm } from "react-hook-form"
+import Button from "@material-ui/core/Button"
+import Icon from "@material-ui/core/Icon"
+
+import "styles/pages/contact.scss"
 
 import Toolbar from "components/Header/Toolbar/Toolbar.jsx"
 import Footer from "components/Footer/Footer.jsx"
@@ -6,6 +11,7 @@ import Backdrop from "components/Backdrop/Backdrop.jsx"
 import SideDrawer from "components/Header/SideDrawer/SideDrawer.jsx"
 
 const Contact = props => {
+  const { register, handleSubmit, errors } = useForm()
   const [isDrawerOpen, setIsDrawerOpen] = useState(false)
 
   const toggleDrawer = () => {
@@ -16,15 +22,66 @@ const Contact = props => {
     toggleDrawer()
   }
 
+  const onSubmit = data => {
+    console.log(data)
+  }
+
   return (
-    <div style={{ height: "100%" }}>
-      <Toolbar openDrawer={toggleDrawer} />
+    <div className="page">
+      <Toolbar openDrawer={toggleDrawer} page="contact" />
       {isDrawerOpen && <SideDrawer show={isDrawerOpen} />}
       {isDrawerOpen && <Backdrop click={backdropClickHandler} />}
-      <main style={{ marginTop: "6rem" }}>
-        <h1>Contact</h1>
+      <main className="main">
+        <div className="header">
+          <h1>Reach Out!</h1>
+          <p>
+            Do contact me if you have internship opportunities or potential
+            freelance projects. I would love to work together with you. If you
+            simply wish to connect, feel free to get in touch as well.
+          </p>
+        </div>
+        <form>
+          <div className="input-container">
+            <label htmlFor="name">Name</label>
+            <input id="name" name="name" ref={register({ required: true })} />
+            {errors.name && <p>Required</p>}
+          </div>
+          <div className="input-container">
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              name="email"
+              ref={register({
+                required: "Required",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "Invalid email address",
+                },
+              })}
+            />
+            {errors.email && <p>{errors.email.message}</p>}
+          </div>
+          <div className="input-container">
+            <label htmlFor="message">Message</label>
+            <textarea
+              id="message"
+              name="message"
+              ref={register({ required: true })}
+            ></textarea>
+
+            {errors.message && <p>Required</p>}
+          </div>
+          <Button
+            variant="contained"
+            color="primary"
+            endIcon={<Icon>send</Icon>}
+            onClick={handleSubmit(onSubmit)}
+          >
+            Submit
+          </Button>
+        </form>
       </main>
-      <Footer />
+      <Footer page="contact" />
     </div>
   )
 }
