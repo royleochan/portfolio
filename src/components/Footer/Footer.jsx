cf1c22b80444feb4dc3a7dbd3d6af21515dac973
royleochan <royleochan@gmail.com>
2020-08-08 03:07:05 +0800

    Add github icon to footer and adjust styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,8 +5,11 @@ import "./index.scss"
 import InstagramIcon from "@ant-design/icons/InstagramOutlined"
 import FacebookIcon from "@ant-design/icons/FacebookOutlined"
 import LinkedInIcon from "@ant-design/icons/LinkedinOutlined"
+import GithubIcon from "@ant-design/icons/GithubOutlined"
 
 const Footer = props => {
+  const { page } = props
+
   const clickHandler = url => {
     window.open(url, "_blank")
   }
@@ -15,19 +18,23 @@ const Footer = props => {
     <div className="footer">
       <footer>
         <div className="icon-container">
-          <InstagramIcon
-            className="icon"
-            onClick={() => clickHandler("https://www.instagram.com/leoroyy/")}
-          />
-          <FacebookIcon
-            className="icon"
-            onClick={() => clickHandler("https://www.facebook.com/royXDXD")}
+          <GithubIcon
+            className={`icon-${page}`}
+            onClick={() => clickHandler("https://github.com/royleochan")}
           />
           <LinkedInIcon
-            className="icon"
+            className={`icon-${page}`}
             onClick={() =>
               clickHandler("https://www.linkedin.com/in/roy-chan-579124176/")
             }
+          />
+          <InstagramIcon
+            className={`icon-${page}`}
+            onClick={() => clickHandler("https://www.instagram.com/leoroyy/")}
+          />
+          <FacebookIcon
+            className={`icon-${page}`}
+            onClick={() => clickHandler("https://www.facebook.com/royXDXD")}
           />
         </div>
       </footer>
