445b10c10f4eb7f6a40486f910acb5a08149722e
royleochan <royleochan@gmail.com>
2020-08-09 20:40:32 +0800

    Add module model and data

---
+++ src/data/modules.js
@@ -0,0 +1,41 @@
+import Module from "src/models/module"
+
+const MODULES = [
+  new Module("CS1010X", "Programming Methodology", "CS", "Y1SP", 1),
+  new Module("BPM1701", "Calculus and Statistics", "BIZ", "Y1S1", 1),
+  new Module(
+    "BPM1702",
+    "Microsoft Excel and Powerpoint for Business",
+    "BIZ",
+    "Y1S1",
+    1
+  ),
+  new Module("BPM1705", "Understanding how business works", "BIZ", "Y1S1", 1),
+  new Module("CS1231S", "Discrete Structures", "CS", "Y1S1", 1),
+  new Module("CS2030", "Programming Methodology II", "CS", "Y1S1", 2),
+  new Module(
+    "DAO1704",
+    "Decision Analytics using Spreadsheets",
+    "BIZ",
+    "Y1S1",
+    1
+  ),
+  new Module(
+    "ES2660",
+    "Communicating in the information age",
+    "COM",
+    "Y1S1",
+    2
+  ),
+  new Module("GES1028", "Singapore Society", "GE", "Y1S1", 1),
+  new Module("MA1521", "Calculus for computing", "MA", "Y1S1", 1),
+  new Module("STR1000", "Career creation starter workshop", "BIZ", "Y1S1", 1),
+  new Module("ACC1701", "Accounting for decision makers", "BIZ", "Y1S2", 1),
+  new Module("CS2040S", "Data Structures and Algorithms ", "CS", "Y1S2", 2),
+  new Module("CS2100", "Computer Organisation", "CS", "Y1S2", 2),
+  new Module("MA1101R", "Linear Algebra I", "MA", "Y1S2", 1),
+  new Module("GER1000", "Quantitative Reasoning", "GE", "Y1S2", 1),
+  new Module("GEQ1000H", "Asking Questions", "GE", "Y1S2", 1),
+]
+
+export default MODULES
---
+++ src/models/module.js
@@ -0,0 +1,11 @@
+class Module {
+  constructor(moduleCode, title, type, completionDate, level) {
+    this.moduleCode = moduleCode
+    this.title = title
+    this.type = type
+    this.completionDate = completionDate
+    this.level = level
+  }
+}
+
+export default Module
